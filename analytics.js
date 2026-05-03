31c12b5499fc6e6b2cf60b3a34939b843f18fad3
gmunkhuchral-debug <gmunkhuchral@gmail.com>
2026-05-03 21:14:57 +0800

    jkakj

---
+++ analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics 4 bootstrap (extracted to external file for CSP-friendly loading)
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-9BX36FS624');
