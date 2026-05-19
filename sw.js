6affa852631fe095e3488f28db2c43de035f6614
gmunkhuchral-debug <gmunkhuchral@gmail.com>
2026-05-19 08:48:01 +0800

    о

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * <script>navigator.serviceWorker.register('/sw.js')</script> хэсгийг арилгана.
  */
 
-const CACHE_VERSION = 'nomaad-v2.6.0-gallery-header';
+const CACHE_VERSION = 'nomaad-v2.7.0-webhook-chimunllc';
 const STATIC_CACHE  = `nomaad-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `nomaad-runtime-${CACHE_VERSION}`;
 
